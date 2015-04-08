7b336e0110e3a65a1324f89103c5e1d6215ac73a
Kevin Deldycke <kdeldycke@ocs.online.net>
2015-04-08 16:28:22 +0200

    Add support for Croatian VAT numbers.

--- pyvat/__init__.py
+++ pyvat/__init__.py
@@ -33,6 +33,7 @@ VAT_NUMBER_EXPRESSIONS = {
     'FR': re.compile(r'^[\da-hj-np-z]{2}\d{9}$', re.IGNORECASE),
     'GB': re.compile(r'^((\d{9})|(\d{12})|(GD\d{3})|(HA\d{3}))$',
                      re.IGNORECASE),
+    'HR': re.compile(r'^\d{11}$'),
     'HU': re.compile(r'^\d{8}$'),
     'IE': re.compile(r'^((\d{7}[a-z])|(\d[a-z]\d{5}[a-z])|(\d{6,7}[a-z]{2}))$',
                      re.IGNORECASE),
