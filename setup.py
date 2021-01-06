b0462f29cfba61186d49c5e67f114ac52cad9dd7
Jan <Jan@meznik.dk>
2021-01-06 11:46:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("README.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 packages = [
@@ -26,37 +26,28 @@ tests_require = [
 
 setup(
     name='pyvat',
-    version='1.3.23',
+    version='1.3.26',
     description='VAT validation for Python',
-#   long_description="""# Markdown supported!\n\n* Cheer\n* Celebrate\n""",
-#   long_description_content_type='text/markdown',
-
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     author='Iconfinder',
     author_email='support@iconfinder.com',
     url='https://www.iconfinder.com',
-#    project_urls={
-#        'Issue Tracker': 'https://github.com/iconfinder/pyvat/issues',
-#    },
-#    packages=packages,
-#    package_data={'': ['LICENSE']},
-#    package_dir={'pyvat': 'pyvat'},
-#    include_package_data=True,
-#    tests_require=tests_require,
-#    install_requires=requires,
-#    license=open('LICENSE').read(),
-#    zip_safe=True,
-#    classifiers=(
-#        'Development Status :: 5 - Production/Stable',
-#        'Intended Audience :: Developers',
-#        'Natural Language :: English',
-#        'License :: OSI Approved :: Apache Software License',
-#        'Programming Language :: Python',
-#        'Programming Language :: Python :: 2.7',
-#        'Programming Language :: Python :: 3.3',
-#        'Programming Language :: Python :: 3.4',
-#        'Programming Language :: Python :: 3.5',
-#        'Programming Language :: Python :: 3.7',
-#    ),
+    project_urls={
+        'Issue Tracker': 'https://github.com/iconfinder/pyvat/issues',
+    },
+    packages=packages,
+    install_requires=requires,
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.7',
+    ),
 )
