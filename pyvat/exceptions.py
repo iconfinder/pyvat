8fcc91956e3978e3bc3991b2470b02cd48dd545b
Jan <Jan@meznik.dk>
2021-02-03 08:20:51 +0100

    Add arguments to super() call

--- pyvat/exceptions.py
+++ pyvat/exceptions.py
@@ -1,4 +1,4 @@
 class ServerError(Exception):
     def __init__(self, fault_code):
-        super().__init__("ServerError: {}".format(fault_code))
+        super(ServerError, self).__init__("ServerError: {}".format(fault_code))
         self.fault_code = fault_code
