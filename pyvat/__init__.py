7ef62a9e22a450562587b7ae29a3e1e73c0a1a95
rathcke <nikolaj.d.rathcke@gmail.com>
2023-01-03 10:03:16 +0100

    Bump version number

--- pyvat/__init__.py
+++ pyvat/__init__.py
@@ -7,7 +7,8 @@ from .result import VatNumberCheckResult
 from .vat_charge import VatCharge, VatChargeAction
 from .vat_rules import VAT_RULES
 
-__version__ = '1.3.16'
+__version__ = '1.3.18'
+
 
 WHITESPACE_EXPRESSION = re.compile(r'[\s\-]+')
 """Whitespace expression.
